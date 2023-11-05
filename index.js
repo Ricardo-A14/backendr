f3e23099a1cd4667ccd4730f14f364bd04c7ba93
Ricardo Robayo <ricardo@DESKTOP-3OI2EQM.localdomain>
2023-11-05 11:14:36 -0500

    sc

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
-app.get('/reactr/home', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Mi servidor en express.');
 });
 
-app.get('/reactr/products', (req, res) => {
+app.get('/products', (req, res) => {
   res.json([
     { name: 'product 1' },
     { name: 'product 2' },
@@ -21,8 +21,6 @@ app.get('/reactr/products', (req, res) => {
 
 
 
-app.listen(port, () => {
-  console.log('My port: ' + port);
-});
+app.listen(port);
 
 
