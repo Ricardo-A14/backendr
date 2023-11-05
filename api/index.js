b42bc966ed4977681376fa300bf93a6e91a73630
Ricardo Robayo <ricardo@DESKTOP-3OI2EQM.localdomain>
2023-11-05 14:45:49 -0500

    sc

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+
+app.use(cors());
 
 
 app.get('/api', (req, res) => {
