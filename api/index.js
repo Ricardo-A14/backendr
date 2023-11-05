a1e05360110a15583936c78944f25dadee155e15
Ricardo Robayo <ricardo@DESKTOP-3OI2EQM.localdomain>
2023-11-05 11:21:37 -0500

    sc

--- api/index.js
+++ api/index.js
@@ -6,11 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Mi servidor en express.');
 });
 
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
   res.json([
     { name: 'product 1' },
     { name: 'product 2' },
